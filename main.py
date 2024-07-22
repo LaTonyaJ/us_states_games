36df1b56dd1dfcb5304300ab9bcb415b3551e2a6
LaTonya Johnson <latonya.johnson1@walmart.com>
2024-07-22 11:30:24 -0500

    code refactor

--- main.py
+++ main.py
@@ -16,12 +16,16 @@ data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
 guessed_states = []
 game_on = True
-# guessing = True
 
 while(game_on):
     state_guess = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Name the state").title()
     
     if state_guess == "Exit":
+        states_to_learn = [state for state in states if state not in guessed_states]
+
+        # turn states_to_learn dict to csv
+        states_to_learn_df = pandas.DataFrame(states_to_learn)
+        states_to_learn_df.to_csv("states_to_learn.csv")
         break
 
     if (state_guess in states and state_guess not in guessed_states):
@@ -40,12 +44,3 @@ while(game_on):
         guessed_states.append(state_guess)
         # print("guessed_states:", guessed_states)
 
-#convert unguessed states to csv
-states_to_learn = []
-for state in states:
-    if state not in guessed_states:
-        states_to_learn.append(state)
-
-# turn states_to_learn dict to csv
-states_to_learn_df = pandas.DataFrame(states_to_learn)
-states_to_learn_df.to_csv("states_to_learn.csv")
